c0a54fc4baa23b82966eea59e14fd9732f19836b
Roberto <147996450+Roberto353656@users.noreply.github.com>
2024-02-20 05:00:48 -0300

    Update Hm.lua

--- Hm.lua
+++ Hm.lua
@@ -3,22 +3,26 @@ Oi gente, não sei se vocês vão ver isso, mas provavelmente vão, não sei por
 Indo direto ao assunto, provavelmente perceberam que estou um pouco mais afastado. Não sei a resposta, mas tudo bem. Gosto muito de vocês, mas ouvir coisas desagradáveis da pessoa que gosto não é legal, sabe? Sempre fui tranquilo sobre esses assuntos, mas isso machuca muito...
 
 Não sou a melhor pessoa do mundo, posso não ter sido um bom amigo, pois sou falho em muitos aspectos. Caso tenha sido um pouco idiota em algum momento, sinto muito. Não vou pedir desculpas porque foi um erro meu, e sempre vou assumir meus erros. Espero que vocês fiquem bem, que suas vidas sejam alegres e cheias de felicidades. ❤️
-
-            
+--
+--
 if
-
-
-                  
+--
+--
+-----
 i = AddButton("To Raissa"function()
 https://open.spotify.com/track/6U9STPNloFqjoSsDrdHmRH?si=E28vvgoxTWGdBb8jbiyV_w
       end)
  Print("Raissa, sinto muito por tudo, por todas as bobagens que fiz, por todas as vezes que te fiz passar vergonha, por cada coisa estúpida que possa ter feito. Entre todas as pessoas no mundo, nunca me senti tão bem como no tempo que estive contigo. Saiba que gostei muito de você, mesmo que você não tenha sentido o mesmo por mim. Eu poderia escrever um texto extenso, mas não quero tomar seu tempo, então vou resumir. Quando te conheci, mesmo sem intenção, acabei realmente gostando de você. Para mim, foi como se tivesse encontrado minha outra metade. Contigo, me sinto alguém tão livre. Sempre quis encontrar alguém com quem pudesse ser eu mesmo, ser o ser mais abominável deste mundo sem ser julgado. No entanto, após um tempo, acabei me machucando um pouco. Você não respondia, mas aparecia online. Eu dizia a mim mesmo que você estava ocupada, que tinha seus motivos, mas, no final, só fiquei mais ansioso. Cada mensagem sua parecia uma pontada enorme no meu coração, como se qualquer coisa pudesse ser motivo para o fim. Isso realmente me afetou, mas tudo bem, você tem suas coisas, eu entendo.")
-    else    
-
-
-
-
-
-                  
+--
+--
+   else    
+--
+--
+--
+--
+--
+--    
      local players = game:GetService("players")
+--
+--
       print("Me desculpem, pessoal, mas vou dar um tempo da internet. Sinto muito por qualquer erro meu. Se quiserem me remover da amizade no Roblox ou no Discord, tudo bem. Não vou forçar vocês a ficarem perto de mim. Sou um garoto emotivo pra caceta, então espero que todos sejam felizes de coração. ❤️")
